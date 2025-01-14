269da694a2248875bff7d37e1ee54245984419f0
dnoemie <noemie.dublanc@ynov.com>
2025-01-14 19:18:57 +0100

    push gameplay, infini loop, obstacle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,79 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameManager Instance { get; private set; }
+
+    public float initialGameSpeed = 5f;
+    public float gameSpeedIncrease = 0.1f;
+    public float gameSpeed { get; private set; }
+
+    private Player player;
+    private Spawner spawner;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        // Utiliser FindFirstObjectByType pour récupérer un seul objet
+        player = FindFirstObjectByType<Player>();
+        spawner = FindFirstObjectByType<Spawner>();
+
+        if (player == null || spawner == null)
+        {
+            Debug.LogError("Player ou Spawner introuvable dans la scène.");
+            return;
+        }
+
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        // Utiliser FindObjectsByType pour récupérer tous les obstacles
+        GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+
+
+        foreach (var obstacle in obstacles)
+        {
+            Destroy(obstacle.gameObject);
+        }
+
+        gameSpeed = initialGameSpeed;
+        enabled = true;
+
+        player.gameObject.SetActive(true);
+        spawner.gameObject.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        gameSpeed = 0f;
+        enabled = false;
+
+        player.gameObject.SetActive(false);
+        spawner.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        gameSpeed += gameSpeedIncrease * Time.deltaTime;
     }
 }
